5464ef377f899bd4b18da97dcb60603ad0ee0d0e
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-10-05 07:18:15 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  for(i=0;i<str.length;i++)
-  {
-      for(j=0;j<bracketsConfig.length;j++)
-      {
-          if(str[i] === bracketsConfig[j][0] && str[i+1] === bracketsConfig[j][1])
-          {
-              str.slice(i,2);
-              i = 0;
-          }
-      }
-  }
-    return str.length===0?true:false;
+    
+let newstr = str.split('');
+
+	for (i = 0; i< str.length; i++) {
+		for (j = 0; j < bracketsConfig.length; j++) {
+			if (str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) {
+				newstr = newstr.join('');
+        newstr = newstr.replace(str[i],'');
+        newstr = newstr.replace(str[i+1],'');
+        i = 0;
+			}
+		}
+	}
+
+	return newstr.length === 0?true:false;
+    
 }
