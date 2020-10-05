55ab8d3ff37bc79e0c6f4e79d87921a7f2141896
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-10-05 08:52:20 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ module.exports = function check(str, bracketsConfig) {
     if (str.length % 2 !== 0) 
         return false;
      str = str.split('');
-    for ( i = 0; i < str.length; i++) {
-        for (j = 0; j < bracketsConfig.length; j++) {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
             if(str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) {
                 str.splice(i, 2);
                 i -=2;
             }
         }
     }
-    return (arr.length == 0) ? true : false;
+    return (str.length == 0) ? true : false;
 }
