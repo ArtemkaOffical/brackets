20f13b400552f3db73c24af7a4a95c4efd75225c
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-10-05 07:00:01 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(i=0;i<str.length;i++)
+  {
+      for(j=0;j<bracketsConfig.length;j++)
+      {
+          if(str[i] === bracketsConfig[j][0] && str[i+1] === bracketsConfig[j][1])
+          {
+              str.splice(i,2);
+              i = 0;
+          }
+      }
+  }
+    return str.length===0?true:false;
 }
